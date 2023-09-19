7b619a21968a6adffd41936ddfd1eb6270ed878e
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-09-18 22:44:21 -0400

    Sets up scaffolding for sidebar plus static data

---
+++ src/components/SidebarData.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <FaIcons.FaHome />
+  },
+  {
+    title: "Candy",
+    path: "/candy",
+    icon: <FaIcons.FaCandyCane />
+  },
+  {
+    title: "Refrigerated",
+    path: "/refrigerated",
+    icon: <FaIcons.FaIceCream />
+  },
+  {
+    title: "Frozen",
+    path: "/frozen",
+    icon: <FaIcons.FaSnowflake />
+  },
+  {
+    title: "Spicy",
+    path: "/spicy",
+    icon: <FaIcons.FaFire />
+  }
+]
