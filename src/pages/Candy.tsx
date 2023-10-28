9a71da92ebf07025387568fb80a8ee9b847bdbab
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-10-28 13:29:49 -0400

    Adds columns and rows for more control of layout

--- src/pages/Candy.tsx
+++ src/pages/Candy.tsx
@@ -2,22 +2,31 @@ import React from "react";
 import { SaleItem } from "../components/SaleItem";
 import { CandyDataTemp } from "./CandyDataTemp";
 import { PageTitle } from "../components/PageTitle";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-const Candy: React.FunctionComponent = () => {
+const Candy = () => {
   return (
-  <>
-  <PageTitle title="Candy" />
-  {CandyDataTemp.map((item, index) => {
-    return (
-      <SaleItem
-        image={item.image}
-        price={item.price}
-        label={item.label}
-        description={item.description}
-      />
-    );
-  })}
-  </>
-)};
+    <>
+      <PageTitle title="Candy" />
+      <Container>
+        <Row style={{ display: "flex" }}>
+          {CandyDataTemp.map((item, index) => {
+            return (
+              <Col sm>
+                <SaleItem
+                  image={item.image}
+                  price={item.price}
+                  label={item.label}
+                  description={item.description}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );};
 
 export default Candy;
