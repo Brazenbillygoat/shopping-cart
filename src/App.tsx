1b2c3f258ecd408ef4a2184b2f64a1ded7c97d33
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-10-28 13:23:22 -0400

    Cleans up some frivilous boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { ThemeProvider } from "./providers/theme-provider";
 import Header from "./components/Header";
@@ -25,20 +24,6 @@ const App = () => {
         <Route path="/Frozen" element={<Frozen />} />
         <Route path="/Spicy" element={<Spicy />} />
       </Routes>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 };
