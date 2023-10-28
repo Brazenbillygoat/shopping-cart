58817197a08734c9560933ebef816d48389c5fd9
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-10-28 13:30:00 -0400

    formatting

--- src/pages/CandyDataTemp.tsx
+++ src/pages/CandyDataTemp.tsx
@@ -1,32 +1,36 @@
 export const CandyDataTemp = [
   {
-    image: "https://cdn.pixabay.com/photo/2015/03/26/23/09/cake-pops-693645_1280.jpg",
-    price: 3.00,
+    image:
+      "https://cdn.pixabay.com/photo/2015/03/26/23/09/cake-pops-693645_1280.jpg",
+    price: 3.0,
     label: "Cake Pops",
-    description: "Cake on a stick. Approximately 1-2 bites depending on the size of your mouth and your sweet tooth.",
+    description:
+      "Cake on a stick. Approximately 1-2 bites depending on the size of your mouth and your sweet tooth.",
   },
   {
-    image: "https://cdn.pixabay.com/photo/2013/08/10/18/13/candies-171342_1280.jpg",
-    price: 4.00,
+    image:
+      "https://cdn.pixabay.com/photo/2013/08/10/18/13/candies-171342_1280.jpg",
+    price: 4.0,
     label: "Gummies",
-    description: "Gummies are a break from chocolate or chocolate is a break from gummies?",
+    description:
+      "Gummies are a break from chocolate or chocolate is a break from gummies?",
   },
   {
     image: "url",
-    price: 5.00,
+    price: 5.0,
     label: "",
     description: "",
   },
   {
     image: "url",
-    price: 6.00,
+    price: 6.0,
     label: "",
     description: "",
   },
   {
     image: "url",
-    price: 2.00,
+    price: 2.0,
     label: "",
     description: "",
   },
-]
+];
