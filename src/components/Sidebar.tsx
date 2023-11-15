3f37f9d1b2c06d68b3354b18cb0725097c6ecff6
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-11-15 08:02:53 -0500

    styles the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,99 +3,46 @@ import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
 import { SidebarData } from "./SidebarData";
 import styled from "styled-components";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
-const Navbar = styled.div`
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  height: 3.5rem;
-  background-color: #000080;
-`;
+const Sidebar = () => {
+  const [show, setShow] = useState(false);
 
-const MenuIconOpen = styled(Link)`
-  display: flex;
-  justify-content: start;
-  font-size: 1.5rem;
-  margin-left: 2rem;
-  color: #ffffff;
-`;
-
-const MenuIconClose = styled(Link)`
-  display: flex;
-  justify-content: end;
-  font-size: 1.5rem;
-  margin-top: 0.75rem;
-  margin-right: 1rem;
-  color: #ffffff;
-`;
-
-const SidebarMenu = styled.div<{ close: boolean }>`
-  width: 250px;
-  height: 100vh;
-  background-color: #000080;
-  position: fixed;
-  top: 0;
-  left: ${({ close }) => (close ? "0" : "-100%")};
-  transition: 0.6s;
-`;
-
-const MenuItems = styled.li`
-  list-style: none;
-  display: flex;
-  align-items: center;
-  justify-content: start;
-  width: 100%;
-  height: 90px;
-  padding: 1rem 0 1.25rem;
-`;
-
-const MenuItemLinks = styled(Link)`
-  display: flex;
-  align-items: center;
-  padding: 0 2rem;
-  font-size: 20px;
-  text-decoration: none;
-  color: #ffffff;
-
-  &:hover {
-    background-color: #ffffff;
-    color: #000080;
-    width: 100%;
-    height: 45px;
-    text-align: center;
-    border-radius: 5px;
-    margin: 0 2rem;
-  }
-`;
-
-const Sidebar: React.FunctionComponent = () => {
-  const [close, setClose] = useState(false);
-  const showSidebar = () => setClose(!close);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-      <Navbar>
-        <MenuIconOpen to="#" onClick={showSidebar}>
-          <FaIcons.FaBars />
-        </MenuIconOpen>
-      </Navbar>
+      <Button className="menu-button sidebar-open-button" variant="primary" onClick={handleShow}>
+        Menu&nbsp;<FaArrowRightLong />
+      </Button>
 
-      <SidebarMenu close={close}>
-        <MenuIconClose to="#" onClick={showSidebar}>
-          <FaIcons.FaTimes />
-        </MenuIconClose>
-
-        {SidebarData.map((item, index) => {
-          return (
-            <MenuItems key={index}>
-              <MenuItemLinks to={item.path}>
-                {item.icon}
-                <span style={{ marginLeft: "16px" }}>{item.title}</span>
-              </MenuItemLinks>
-            </MenuItems>
-          );
-        })}
-      </SidebarMenu>
+      <Offcanvas className="sidebar" show={show} onHide={handleClose}>
+        <Offcanvas.Header className="sidebar-header">
+          <h1 className="sidebar-title">Categories:</h1>
+          <Button className="sidebar-close-button" onClick={handleClose}>
+            <FaArrowLeftLong />
+          </Button>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ul className="sidebar-links-container">
+            {SidebarData.map((item, index) => {
+              return (
+                <Link
+                  className="nav-link sidebar-link"
+                  to={item.path}
+                  key={index}
+                >
+                  {item.title}
+                </Link>
+              );
+            })}
+          </ul>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 };
