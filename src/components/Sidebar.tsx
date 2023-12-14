ef1a958a94ecf2660c828876502e787d68743ecf
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-12-14 10:48:56 -0500

    styles sidebar close button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 const Sidebar = () => {
@@ -11,48 +13,23 @@ const Sidebar = () => {
 
   return (
     <>
-      <button
-        className="btn btn-primary"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasWithBackdrop"
-        aria-controls="offcanvasWithBackdrop"
+      <Button
+        className="menu-button sidebar-open-button"
+        variant="primary"
+        onClick={handleShow}
       >
-        Enable backdrop
-      </button>
-
-      <div
-        className="offcanvas offcanvas-start"
-        tabIndex={-1}
-        id="offcanvasWithBackdrop"
-        aria-labelledby="offcanvasWithBackdropLabel"
-      >
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasWithBackdropLabel">
-            Offcanvas with backdrop
-          </h5>
-          <button
-            type="button"
-            className="btn-close text-reset"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          >
-            <FaArrowLeftLong />
-          </button>
-        </div>
-        <div className="offcanvas-body">
-          <p>I'm the offCanvas body</p>
-        </div>
-      </div>
-      {/* <button className="menu-button sidebar-open-button" variant="primary" onClick={handleShow}>
-        Menu&nbsp;<FaArrowRightLong />
-      </button>
+        Menu&nbsp;
+        <FaArrowRightLong />
+      </Button>
 
       <Offcanvas className="sidebar" show={show} onHide={handleClose}>
         <Offcanvas.Header className="sidebar-header">
           <h1 className="sidebar-title">Categories:</h1>
           <Button className="sidebar-close-button" onClick={handleClose}>
-            <FaArrowLeftLong />
+              <FaArrowLeftLong
+                style={{ marginBottom: "-.1rem", marginRight: ".1rem" }}
+              />
+              Close
           </Button>
         </Offcanvas.Header>
         <Offcanvas.Body>
@@ -70,7 +47,7 @@ const Sidebar = () => {
             })}
           </ul>
         </Offcanvas.Body>
-      </Offcanvas> */}
+      </Offcanvas>
     </>
   );
 };
