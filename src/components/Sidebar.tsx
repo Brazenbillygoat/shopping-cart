c993fc93310e3e9e23c61c342e9b4cc52bea2050
Hyrum Butler <hyrumbutler@hyrums-mbp.mynetworksettings.com>
2023-12-20 13:47:05 -0500

    sidebar now closes when focus is lost

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 const Sidebar = () => {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setTimeout(() => setShow(false), 3);
+  const handleClose = () => setTimeout(() => setShow(false), 30);
   const handleShow = () => setShow(true);
 
   return (
